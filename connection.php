76f87d0b39f554163a1a1780a6c814ef9c5fbbee
maulanasatyaadi <maulanasatyaadi@gmail.com>
2016-09-24 16:53:33 +0700

    update comment
    create an user guide to use this script

--- connection.php
+++ connection.php
@@ -1,4 +1,15 @@
 <?php
+/*
+   This script is a part of query.php.
+   This script containing configuration that will be used
+   on query.php operation.
+   
+   $config['host'] = 'localhost' // mysql server url
+   $config['username'] = 'project_16' // mysql username
+   $config['password'] = 'maulana' // mysql password
+   $config['database'] = 'project_16' // mysql database
+*/
+
 $config = array();
 
 $config['host'] = 'localhost';
